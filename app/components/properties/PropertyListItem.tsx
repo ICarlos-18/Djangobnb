9f46f6cad0bb37df53fff552de62d653ef4664cd
ICarlos-18 <csanchezhernandez567@gmail.com>
2025-04-11 16:32:08 -0600

    15 final commits

--- app/components/properties/PropertyListItem.tsx
+++ app/components/properties/PropertyListItem.tsx
@@ -1,23 +1,40 @@
 import Image from "next/image";
 import { PropertyType } from "./PropertyList";
+import { useRouter } from "next/navigation";
+import FavoriteButton from "../FavoriteButton";
 
 interface PropertyProps {
-    property: PropertyType;
+    property: PropertyType,
+    markFavorite?: (is_favorite: boolean) => void;
 }
 
 const PropertyListItem: React.FC<PropertyProps> = ({
-    property
+    property,
+    markFavorite
 }) => {
+    const router = useRouter();
+
     return (
-        <div className="cursor-pointer">
+        <div 
+            className="cursor-pointer"
+            onClick={() => router.push(`/properties/${property.id}`)}
+        >
             <div className="relative overflow-hidden aspect-square rounded-xl">
-                <Image 
+                <Image
                     fill
                     src={property.image_url}
                     sizes="(max-width: 768px) 768px, (max-width: 1200px): 768px, 768px"
                     className="hover:scale-110 object-cover transition h-full w-full"
                     alt="Beach house"
-                /> 
+                />
+
+                {markFavorite && (
+                    <FavoriteButton
+                        id={property.id}
+                        is_favorite={property.is_favorite}
+                        markFavorite={(is_favorite) => markFavorite(is_favorite)}
+                    />
+                )}
             </div>
 
             <div className="mt-2">
@@ -25,7 +42,7 @@ const PropertyListItem: React.FC<PropertyProps> = ({
             </div>
 
             <div className="mt-2">
-                <p className="text-sm text-gray-500"><strong>${property.price_per_night}</strong> for night</p>
+                <p className="text-sm text-gray-500"><strong>${property.price_per_night}</strong> per night</p>
             </div>
         </div>
     )
